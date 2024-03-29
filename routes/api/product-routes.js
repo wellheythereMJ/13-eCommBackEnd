7ae061e0c82a8dab5891105ba247c77a53aaf839
wellheythereMJ <abetterplace@outlook.com>
2024-03-29 10:45:27 -0700

    delete path completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag, 
+          through: ProductTag,
+        },
+      ],
+    }
+  ).then((products) => {
+    res.status(200).json(products);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+      },
+      {
+        model: Tag, 
+        through: ProductTag,
+      },
+    ],
+  }).then((products) => {
+    res.status(200).json(products);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -94,6 +128,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+   .then((product) => {
+      return res.json(product);
+    })
+   .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
